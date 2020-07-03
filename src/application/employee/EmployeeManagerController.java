54921fdd007f6df112b38130634a506ad18a65f8
vuhaile <levuhai207@hotmail.com.vn>
2020-07-03 14:39:19 +0700

    Merge branch 'master' of https://github.com/hailv207/Module2-CaseStudy into hai
    # Conflicts:
    #	src/application/employee/data/employees.dat
    update Employee Manager Functions

--- src/application/employee/EmployeeManagerController.java
+++ src/application/employee/EmployeeManagerController.java
@@ -38,14 +38,9 @@ public class EmployeeManagerController implements Initializable {
     private TableColumn<Employee, String> accessTypeColumn;
     @FXML
     private TableColumn<Employee, Boolean> statusColumn;
-    @FXML
-    private TableColumn<Employee, String> usernameColumn;
-
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        writeEmployees();
-        readEmployees();
         codeColumn.setCellValueFactory(new PropertyValueFactory<Employee, String>("code"));
         nameColumn.setCellValueFactory(new PropertyValueFactory<Employee, String>("name"));
         idNumberColumn.setCellValueFactory(new PropertyValueFactory<Employee, String>("idNumber"));
@@ -53,42 +48,42 @@ public class EmployeeManagerController implements Initializable {
         birthdayColumn.setCellValueFactory(new PropertyValueFactory<Employee, LocalDate>("birthday"));
         accessTypeColumn.setCellValueFactory(new PropertyValueFactory<Employee, String>("accessType"));
         statusColumn.setCellValueFactory(new PropertyValueFactory<Employee, Boolean>("status"));
-        table.setItems(EmployeeManager.getEmployees());
+        table.getItems().addAll(EmployeeManager.getEmployees());
     }
 
-    public void writeEmployees() {
-        String filePath = "src/application/employee/data/employees.dat";
-        File file = new File(filePath);
-        try {
-            FileOutputStream fos = new FileOutputStream(file);
-            ObjectOutputStream oos = new ObjectOutputStream(fos);
-            oos.writeObject(new Employee("hailv", "Le Vu Hai", "Ha noi", "199199", "Manager", "hailv", true));
-            oos.writeObject(new Employee("tung", "Nguyen Tuan Tung", "Ha noi", "122222", "Staff", "tung", true));
-            oos.writeObject(new Employee("hailv", "Nguyen Tuan Toan", "Ha noi", "354646", "Staff", "toan", true));
-            fos.close();
-            oos.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+//    public void writeEmployees() {
+//        String filePath = "src/application/employee/data/employees.dat";
+//        File file = new File(filePath);
+//        try {
+//            FileOutputStream fos = new FileOutputStream(file);
+//            ObjectOutputStream oos = new ObjectOutputStream(fos);
+//            oos.writeObject(new Employee("hailv", "Le Vu Hai", "Ha noi", "199199", "Manager", "hailv", true));
+//            oos.writeObject(new Employee("tung", "Nguyen Tuan Tung", "Ha noi", "122222", "Staff", "tung", true));
+//            oos.writeObject(new Employee("hailv", "Nguyen Tuan Toan", "Ha noi", "354646", "Staff", "toan", true));
+//            fos.close();
+//            oos.close();
+//        } catch (Exception e) {
+//        }
+//    }
 
-    public void readEmployees() {
-        writeEmployees();
-        String filePath = "src/application/employee/data/employees.dat";
-        File file = new File(filePath);
-        try {
-            FileInputStream fis = new FileInputStream(file);
-            ObjectInputStream ois = new ObjectInputStream(fis);
-            Object obj = null;
-            while ((obj = ois.readObject()) != null) {
-                EmployeeManager.addNewEmployee((Employee) obj);
-            }
-            fis.close();
-            ois.close();
-        } catch (Exception e) {
-             e.printStackTrace();
-        }
-    }
+//    public void readEmployees() {
+//        writeEmployees();
+//        String filePath = "src/application/employee/data/employees.dat";
+//        File file = new File(filePath);
+//        try {
+//            FileInputStream fis = new FileInputStream(file);
+//            ObjectInputStream ois = new ObjectInputStream(fis);
+//            Object obj = null;
+//            while ((obj = ois.readObject()) != null) {
+//                EmployeeManager.addNewEmployee((Employee) obj);
+//            }
+//            fis.close();
+//            ois.close();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+
     public void changeSceneEditEmployee(ActionEvent event) throws IOException {
         Stage stage = (Stage) (((Node) event.getSource()).getScene().getWindow());
         FXMLLoader loader = new FXMLLoader();
